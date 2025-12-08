a5a0f220a05fc67ddca15b05256d086788c44759
ruben <rubenbabu7993@.gmail.com>
2025-12-09 01:11:02 +0530

    Add project setup files and source directory

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ## End to End Machine learing Projects
+g
---
+++ setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+    
+    if '-e .' in requirements:
+        requirements.remove('-e .')
+    
+    return requirements
+
+setup(
+    name='mlprojects',
+    version='0.0.1',
+    author='ruben',
+    author_email='rubenbabu7993@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
+"
